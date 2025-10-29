93d2fade28194217c6a51c053fae2fb58cdf3ad5
murilo <112010004+murilott@users.noreply.github.com>
2025-10-29 00:15:55 +0000

    feat: criação do serviço Quadra

---
+++ quadra/src/main/java/com/example/quadra/QuadraApplication.java
@@ -0,0 +1,13 @@
+package com.example.quadra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadraApplication.class, args);
+	}
+
+}
---
+++ quadra/src/test/java/com/example/quadra/QuadraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quadra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
