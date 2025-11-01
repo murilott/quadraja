9c4882310dc0d810298fa805aa6e1a9d5d9b98fa
murilo <112010004+murilott@users.noreply.github.com>
2025-11-01 16:25:04 +0000

    feat: rota home

--- quadra/src/main/java/com/example/quadra/interfaces/rest/HelloController.java
+++ quadra/src/main/java/com/example/quadra/interfaces/rest/HelloController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @GetMapping("/")
+    public String home() {
+        return "Serviço de quadra. Rotas: GET /quadras: lista; POST /quadras: registrar quadra";
+    }
+
     @GetMapping("/quadra")
     public String hello() {
         return "Serviço de quadra";
