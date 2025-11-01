fd263ef1527508c2ea5122f7a5f191cadf095405
murilo <112010004+murilott@users.noreply.github.com>
2025-11-01 16:37:52 +0000

    fix: correção da validação do RegisterQuadraResponse

--- quadra/src/main/java/com/example/quadra/interfaces/rest/dto/quadra/RegisterQuadraRequest.java
+++ quadra/src/main/java/com/example/quadra/interfaces/rest/dto/quadra/RegisterQuadraRequest.java
@@ -4,12 +4,14 @@ import com.example.quadra.domain.quadra.vo.CategoryType;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record RegisterQuadraRequest(
         @NotBlank String name,
         @NotBlank String local,
-        @NotBlank double price,
-        @NotBlank CategoryType category
+        @Positive double price,
+        @NotNull CategoryType category
 ) {
 }
