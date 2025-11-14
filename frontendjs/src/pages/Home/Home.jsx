0ea1ba2f1229f4f7e004eba8aeb6f139f8c2e6f5
João Pedro Binelo <112009925+Binelo@users.noreply.github.com>
2025-11-14 23:21:01 +0000

    home

--- frontendjs/src/pages/Home/Home.jsx
+++ frontendjs/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import { HomeContainer, HomeContent, Quadra } from "./HomeStyled";
 
-const quadras = [{id: 1, name: "Quadra 1", alugado: true, local: "Pátio 1", price: 100, category: "Basquete"}, {id: 2, name: "Quadra 2", alugado: false, local: "Pátio 3", price: 200, category: "Vôlei"}]
+const quadras = [{id: 1, name: "Quadra 1", alugado: true, local: "Pátio 1", price: 100, category: "Basquete"}, {id: 2, name: "Quadra 2", alugado: false, local: "Pátio 3", price: 200, category: "Vôlei"}, {id: 3, name: "Quadra 3", alugado: true, local: "Pátio 3", price: 120, category: "Basquete"}, {id: 4, name: "Quadra 4", alugado: false, local: "Pátio 4", price: 230, category: "Vôlei"}]
 
 export function Home() {
     return (
@@ -8,14 +8,16 @@ export function Home() {
             <HomeContent>
                 {quadras && quadras.length > 0 && (
                     quadras.map((quadra) => (
-                        <Quadra>
+                        <Quadra className={quadra.alugado ? "alugado" : ""} >
                             <h2>{quadra.name}</h2>
                             <div>
                                 <h4>{quadra.local}</h4>
                             <h3>{quadra.category}</h3>
                             </div>
                             <h6>{quadra.price}</h6>
-                            <span>ALUGADO!</span>
+                            {quadra.alugado && (
+                                <span>ALUGADO!</span>
+                            )}
                         </Quadra>
                     ))
                 )}
--- frontendjs/src/pages/Home/HomeStyled.jsx
+++ frontendjs/src/pages/Home/HomeStyled.jsx
@@ -6,18 +6,73 @@ export const HomeContainer = styled.section`
     margin: 40px auto 0;
 `;
 export const HomeContent = styled.div`
-    box-shadow: 0 6px 15px #00000052;
+    box-shadow: 0 4px 15px #0000003d;
     border-radius: 15px;
-    padding: 40px;
+    padding: 60px 40px;
     width: 100%;
     display: grid;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+    justify-items: center;
+    gap: 20px;
+    
+    @media only screen and (max-width:1055px) {
+        grid-template-columns: 1fr 1fr;
+    }
+    @media only screen and (max-width:700px) {
+        grid-template-columns: 1fr;
+    }
+
 `;
 export const Quadra = styled.div`
-    background-color: var(--dark);
-    color: var(--light);
+    background-color: var(--light);
+    color: var(--dark);
+    box-shadow: 0 5px 15px 0 #00000052, 0 0 0 0 var(--dark) inset;
     padding: 20px;
     border-radius: 10px;
     max-width: 400px;
+    transition: all.3s;
+    position: relative;
     width: 100%;
+    h2 {
+        font-size: 25px;
+        font-weight: 600;
+        color: #5dd60c;
+    }
+    &:hover {
+        box-shadow: 0 0 0 0 #00000052, 0 0 10px 100px var(--dark) inset;
+        color: var(--light);
+    }
+    &.alugado {
+        background-color: #e6e6e6e2;
+        h2 {
+            color: red;
+        }
+        &::before {
+                content: "";
+                width: 100%;
+                height: 100%;
+                background-color: #0000006a;
+                display: block;
+                position: absolute;
+                top: 0;
+                left: 0;
+                border-radius: 10px;
+                z-index: 2;
+            }
+        span {
+            position: absolute;
+            z-index: 3;
+            top: 50%;
+            right: 50%;
+            transform: translate(50%, -50%);
+            background-color: red;
+            color: var(--light);
+            padding: 3px 8px;
+            border-radius: 10px;
+            
+        }
+        &:hover {
+            box-shadow: 0 0 0 0 #00000052, 0 0 0 0 var(--dark) inset;
+        }
+    }
 `;
